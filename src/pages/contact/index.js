62ad76cb5ec5bc86d557a1f8c0f34dc848273152
Cheng Yong Teo <teochengyong@gmail.com>
2019-01-28 16:44:30 +0800

    Disable ajax submission

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -46,7 +46,6 @@ export default class Index extends React.Component {
           action="/contact/thanks/"
           data-netlify="true"
           data-netlify-honeypot="bot-field"
-          onSubmit={this.handleSubmit}
         >
           {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
           <input type="hidden" name="form-name" value="contact" />
